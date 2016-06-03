5ca8243423df89b0473091bbf99d6df024f9d768
Rich Barrett-Small <R.Barrett-Small@sheffield.ac.uk>
2016-06-03 15:47:08 +0100

    Express app

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+// Constants
+var PORT = 3000;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  console.log(req);
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
